8682a3ae6ccdc0e541bc06d9b57a6ce11ff18b3a
Sasha.Solo <sasha.solomannik@gmail.com>
2022-12-17 00:01:09 +0200

    Update Feedback.styled.jsx

--- src/components/FeedbackOption/Feedback.styled.jsx
+++ src/components/FeedbackOption/Feedback.styled.jsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Btn = styled.button`
   padding: 10px;
-  color: #000;
-  background-color: #fff;
+  color: rgb(255, 255, 255);
+  background-color: rgb(247, 64, 2);
   border-radius: 4px;
   border: 1px solid #ccc;
   margin-right: 15px;
@@ -11,7 +11,7 @@ export const Btn = styled.button`
 
   &:hover,
   &:focus {
-    color: #fff;
+    color: rgb(0, 0, 0);
     background-color: #c5c5c5;
     box-shadow: 0px 0px 10px 3px rgba(0, 0, 0, 0.7) inset;
   }
